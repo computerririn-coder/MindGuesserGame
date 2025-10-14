01472c568e04f2a83a84737d8ea41b4b5a42736c
Your Name <your@email.com>
2025-10-14 20:04:01 +0800

    Its finished but i can add more if i want but i want to create a bette new project

--- src/NavigationBar/NavBar.jsx
+++ src/NavigationBar/NavBar.jsx
@@ -3,7 +3,7 @@
 
 
 import { Home, User, Info } from "lucide-react"; // icon library
-import styles from './NavBar.module.css';
+import styles from "./NavBar.module.css";
 
 function Navbar() {
   return (
@@ -11,9 +11,30 @@ function Navbar() {
       <div className={styles.navLogo}>Mind Guesser</div>
 
       <ul className={styles.navMenu}>
-        <li><Home className={styles.navIcon} /><a href="#">Home</a></li>
-        <li><Info className={styles.navIcon} /><a href="#">About</a></li>
-        <li><User className={styles.navIcon} /><a href="#">Login</a></li>
+
+        <li className={styles.tooltip}>
+          <Home className={styles.navIcon} />
+          <a href="#" onClick={() => window.location.reload()}>Home</a>
+          <span className={styles.tooltipText}>Go to Home(idk what to put here so it just refreshed the page)</span>
+        </li>
+
+        <li className={styles.tooltip}>
+          <Info className={styles.navIcon} />
+          <a href="#">About The Code</a>
+          <span className={styles.tooltipText}>
+             This project shows
+             my current skills and how I apply what I know to demonstrate 
+             my abilities to employers.
+          </span>
+        </li>
+
+        <li className={styles.tooltip}>
+          <User className={styles.navIcon} />
+          <a href="#">Login</a>
+          <span className={styles.tooltipText}>
+             i just felt like adding this
+          </span>
+        </li>
       </ul>
     </nav>
   );
